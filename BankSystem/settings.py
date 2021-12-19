8fac0828df236b5af2e4d3105067d62288afb6ee
AniketDohale <88365971+AniketDohale@users.noreply.github.com>
2021-12-19 20:05:35 +0530

    Third Stage

--- BankSystem/settings.py
+++ BankSystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_#xpuywn+vujtlfy(7jneztbpm$^u44i-aolpp_5-lpe!!3)r=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'basicbanksystem01.herokuapp.com']
 
 
 # Application definition
