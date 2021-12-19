0d2e1db67b8b2201c98fc3531965a1c578a29e22
AniketDohale <88365971+AniketDohale@users.noreply.github.com>
2021-12-19 22:26:10 +0530

    Twleveth Stage

--- BankSystem/settings.py
+++ BankSystem/settings.py
@@ -152,4 +152,3 @@ STATICFILES_DIRS = (
 )
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
